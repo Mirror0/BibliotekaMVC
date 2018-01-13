2c566d807a228c6da4d01049fa626894dc301fcf
M B <matias40@vp.pl>
2018-01-13 14:09:22 +0100

    Initial project commit

---
+++ WebApplication1/SQLQuery1.sql
@@ -0,0 +1,114 @@
+﻿create table Autor(
+	ID int check (ID > 0) primary key not null identity,
+    Imie varchar(15),
+    Nazwisko varchar(30)
+    
+);
+
+create table Wydawca(
+	ID int check (ID > 0) primary key not null identity,
+    Nazwa varchar(30)
+);
+
+create table Aktor(
+	ID int check (ID > 0) primary key not null identity,
+    Imie varchar(15),
+    Nazwisko varchar(30),
+);
+
+create table Czytelnik(
+	ID int check (ID > 0) primary key not null identity,
+    Imie varchar(15),
+    Nazwisko varchar(30),
+    Uzytkownik varchar(30),
+    Haslo varchar(255),
+    Email varchar(50)
+);
+
+create table Slowo_Kluczowe(
+	ID int check (ID > 0) primary key not null identity,
+    Strona int check (Strona > 0),
+    Slowo varchar(30)    
+);
+
+create table Film(
+	ID int check (ID > 0) primary key not null identity,
+    Tytul varchar(100),
+	ID_Aktora int check (ID_Aktora > 0),
+	foreign key (ID_Aktora) references Aktor(ID) on delete cascade
+);
+
+create table Praca_Naukowa(
+	ID int check (ID > 0) primary key not null identity,
+	Tytul varchar(100),
+	ID_Autora int check (ID_Autora > 0),
+	foreign key (ID_Autora) references Autor(ID) on delete cascade
+);
+
+
+
+create table Ksiazka(
+	ID int check (ID > 0) primary key not null identity,
+    Tytul varchar(100),
+    ISBN varchar(20),
+    Strony int check (Strony > 0),
+    ID_Wydawcy int check (ID_Wydawcy > 0),
+	ID_Autora int check (ID_Autora > 0),
+	ID_Aktora int check (ID_Aktora > 0),
+	ID_Slowo_Kluczowe int check (ID_Slowo_Kluczowe > 0),
+    foreign key (ID_Wydawcy) references Wydawca(ID) on delete cascade,
+	foreign key (ID_Autora) references Autor(ID) on delete cascade,
+	foreign key (ID_Slowo_Kluczowe) references Slowo_Kluczowe(ID) on delete cascade
+);
+
+create table Czasopismo(
+	ID int check (ID > 0) primary key not null identity,
+    Tytul varchar(100),
+    ISBN varchar(20),
+    Strony int check (Strony > 0),
+    ID_Wydawcy int check (ID_Wydawcy > 0),
+	ID_Autora int check (ID_Autora > 0),
+    foreign key (ID_Wydawcy) references Wydawca(ID) on delete cascade,
+	foreign key (ID_Autora) references Autor(ID) on delete cascade
+
+);
+
+create table Wiadomosci(
+	ID int check (ID > 0) primary key not null identity,
+    Tresc varchar(max),
+    ID_Czytelnika int check (ID_Czytelnika > 0),
+	foreign key (ID_Czytelnika) references Czytelnik(ID) on delete cascade
+);
+
+create table Wypozyczenia_Ksiazki(
+	ID int check (ID > 0) primary key not null identity,
+    ID_Czytelnika int check (ID_Czytelnika > 0),
+    ID_Ksiazki int check (ID_Ksiazki > 0),
+    Data_Wypozyczenia datetime,
+    Data_Zwrotu datetime,
+	foreign key (ID_Czytelnika) references Czytelnik(ID) on delete cascade,
+	foreign key (ID_Ksiazki) references Ksiazka(ID)on delete cascade
+);
+
+create table Wypozyczenia_Filmu(
+	ID int check (ID > 0) primary key not null identity,
+    ID_Czytelnika int check (ID_Czytelnika > 0),
+    ID_Filmu int check (ID_Filmu > 0),
+    Data_Wypozyczenia datetime,
+    Data_Zwrotu datetime,
+	foreign key (ID_Czytelnika) references Czytelnik(ID) on delete cascade,
+    foreign key (ID_Filmu) references Film(ID) on delete cascade
+    
+);
+
+create table Wypozyczenia_Czasopisma(
+	ID int check (ID > 0) primary key not null identity,
+    ID_Czytelnika int check (ID_Czytelnika > 0),
+    ID_Czasopisma int check (ID_Czasopisma > 0),
+    Data_Wypozyczenia datetime,
+    Data_Zwrotu datetime,
+    foreign key (ID_Czytelnika) references Czytelnik(ID) on delete cascade,
+    foreign key (ID_Czasopisma) references Czasopismo(ID) on delete cascade
+);
+
+
---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private LibDBEntities _db = new LibDBEntities();
+        public ActionResult Index()
+        {
+            return View(_db.Aktor.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Details(int id)
+
+        {
+
+            return View();
+
+        }
+
+        //
+
+        // GET: /Home/Create 
+
+        public ActionResult Create()
+
+        {
+
+            return View();
+
+        }
+
+        //
+
+        // POST: /Home/Create 
+
+        [AcceptVerbs(HttpVerbs.Post)]
+
+        public ActionResult Create(FormCollection collection)
+
+        {
+
+            try
+
+            {
+
+                // TODO: Add insert logic here 
+
+                return RedirectToAction("Index");
+
+            }
+
+            catch
+
+            {
+
+                return View();
+
+            }
+
+        }
+
+        //
+
+        // GET: /Home/Edit/5
+
+        public ActionResult Edit(int id)
+
+        {
+
+            return View();
+
+        }
+
+        //
+
+        // POST: /Home/Edit/5 
+
+        [AcceptVerbs(HttpVerbs.Post)]
+
+        public ActionResult Edit(int id, FormCollection collection)
+
+        {
+
+            try
+
+            {
+
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+
+            }
+
+            catch
+
+            {
+
+                return View();
+
+            }
+
+        }
+    }
+}
